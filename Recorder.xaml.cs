c308257405d06e99450e632185dcbb1858f1fe07
chatblanc-ciel <matsumoto9845@spice.ocn.ne.jp>
2021-11-15 21:16:04 +0900

    Change responsive design - ResponsiveGrid - Grid system inspired by Bootstrap - Tab control menu (left side)

--- Recorder.xaml.cs
+++ Recorder.xaml.cs
@@ -144,35 +144,54 @@ namespace SoundReader
             {
                 var current = 100.0 * max;
                 var lv = 0.0;
-                if (mainWindow.Rec_Level_Meter.Value <= current)
+                if (Rec_Level_Meter.Value <= current)
                 {
-                    lv = (0.2 * mainWindow.Rec_Level_Meter.Value + 0.8 * current);
+                    lv = (0.2 * Rec_Level_Meter.Value + 0.8 * current);
                     if (max >= 1.0)
                     {
-                        mainWindow.Rec_Level_Meter.Foreground = new SolidColorBrush((Color)this.Resources["scarlet"]);
+                        Rec_Level_Meter.Foreground = new SolidColorBrush((Color)this.Resources["scarlet"]);
                     }
                     else
                     {
-                        mainWindow.Rec_Level_Meter.Foreground = new SolidColorBrush((Color)this.Resources["light_green"]);
+                        Rec_Level_Meter.Foreground = new SolidColorBrush((Color)this.Resources["light_green"]);
                     }
                 }
                 else
                 {
-                    lv = (0.8 * mainWindow.Rec_Level_Meter.Value + 0.2 * current);
+                    lv = (0.8 * Rec_Level_Meter.Value + 0.2 * current);
                     if (max >= 1.0)
                     {
-                        mainWindow.Rec_Level_Meter.Foreground = new SolidColorBrush((Color)this.Resources["scarlet"]);
+                        Rec_Level_Meter.Foreground = new SolidColorBrush((Color)this.Resources["scarlet"]);
                     }
                     else
                     {
                         var color = (Color)this.Resources["light_green"];
                         color.A = 150;
-                        mainWindow.Rec_Level_Meter.Foreground = new SolidColorBrush(color);
+                        Rec_Level_Meter.Foreground = new SolidColorBrush(color);
                     }
                 }
-                mainWindow.Rec_Level_Meter_Value.Text = lv.ToString("0");
-                mainWindow.Rec_Level_Meter.Value = lv;
+                Rec_Level_Meter_Value.Text = lv.ToString("0");
+                Rec_Level_Meter.Value = lv;
             });
+        }
+    }
+
+    public class CanvasAutoSize : Canvas
+    {
+        protected override System.Windows.Size MeasureOverride(System.Windows.Size constraint)
+        {
+            base.MeasureOverride(constraint);
+            double width = base
+                .InternalChildren
+                .OfType<UIElement>()
+                .Max(i => i.DesiredSize.Width + (double)i.GetValue(Canvas.LeftProperty));
+
+            double height = base
+                .InternalChildren
+                .OfType<UIElement>()
+                .Max(i => i.DesiredSize.Height + (double)i.GetValue(Canvas.TopProperty));
+
+            return new Size(width, height);
         }
     }
 }
