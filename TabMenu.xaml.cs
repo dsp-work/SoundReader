fb23f792eaabd81dd3a0472b62224eaf5e82bd6c
chatblanc-ciel <matsumoto9845@spice.ocn.ne.jp>
2021-11-16 21:35:18 +0900

    Add `Tab Menu Bar`

---
+++ TabMenu.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoundReader
+{
+    /// <summary>
+    /// UserControl1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TabMenu : UserControl
+    {
+        public TabMenu()
+        {
+            InitializeComponent();
+        }
+/*
+        public int MyProperty
+        {
+            get { return (int)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
+
+        */
+    }
+
+    public class test : Behavior<TabControl>
+    {
+
+    }
+}
